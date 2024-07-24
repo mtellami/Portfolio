7f920d8cfe3646d6a8e94a48d3528b25833b3b72
mtellami <moadtell@gmail.com>
2024-07-24 22:02:34 +0100

    init version 2.0.0
    yet another one.

--- src/component/About/About.data.js
+++
@@ -1,6 +0,0 @@
-const data = {
-	header: 'A dedicated Software Developer based in Morocco, Marrakesh 📍',
-	paragraph: 'As a passionate junior web developer with a burning enthusiasm for creating dynamic and engaging web experiences.My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.',
-}
-
-export default data
